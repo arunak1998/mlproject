3713bd4a086d115b3cf384eaefc995b40b0b6ff4
Arunkumar <aktooall@gmail.com>
2024-03-09 19:01:44 +0530

    Project Structure

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+import sys
+from logger import logging 
+def error_handling(error,error_object:sys):
+    _,_,err_track=error_object.exc_info()
+    file_name=err_track.tb_frame.f_code.co_filename
+    # errror_message="Error occured in the Script [{0}] in the line number [{1}] the Error is [{2}]".format(file_name,err_track.tb_lineno,error)
+    error_message = "Error occurred in the Script [{0}] in the line number [{1}] the Error is [{2}]".format(file_name, err_track.tb_lineno, error)
+    return error_message
+
+
+class Customexception(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_handling(error_message,error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+
+
+
